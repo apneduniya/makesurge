067aa4a5f7842b2eacf4342bae4aea917e74af8f
apneduniya <apneduniya.dontdisturb@gmail.com>
2023-04-22 18:50:56 +0530

    Modified

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -47,7 +47,7 @@ const childVariants = {
 const Contact = () => {
     return (
         <>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#f3f4f5" fill-opacity="1" d="M0,96L1440,192L1440,320L0,320Z"></path></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#f3f4f5" fillOpacity="1" d="M0,96L1440,192L1440,320L0,320Z"></path></svg>
             <div
                 className="contact-container"
                 id="contact"
@@ -90,7 +90,7 @@ const Contact = () => {
                     </motion.button>
                 </a>
             </div>
-            {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#f3f4f5" fill-opacity="1" d="M0,96L1440,192L1440,0L0,0Z"></path></svg> */}
+            {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#f3f4f5" fillOpacity="1" d="M0,96L1440,192L1440,0L0,0Z"></path></svg> */}
         </>
     )
 }
--- src/pages/Services.js
+++ src/pages/Services.js
@@ -77,7 +77,7 @@ const ServicesFlip = (props) => {
                             className="services-flip-container-contents"
                             style={{ margin: 0, padding: "12px 12px", backgroundColor: "#012970", width: "min-content", borderRadius: "100%", display: "flex", justifyContent: "center", alignContent: "center" }}
                         >
-                            <svg width="12px" height="12px" viewBox="-19.04 0 75.804 75.804" xmlns="http://www.w3.org/2000/svg" fill="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g id="Group_65" data-name="Group 65" transform="translate(-831.568 -384.448)"> <path id="Path_57" data-name="Path 57" d="M833.068,460.252a1.5,1.5,0,0,1-1.061-2.561l33.557-33.56a2.53,2.53,0,0,0,0-3.564l-33.557-33.558a1.5,1.5,0,0,1,2.122-2.121l33.556,33.558a5.53,5.53,0,0,1,0,7.807l-33.557,33.56A1.5,1.5,0,0,1,833.068,460.252Z" fill="#ffffff"></path> </g> </g></svg>
+                            <svg width="12px" height="12px" viewBox="-19.04 0 75.804 75.804" xmlns="http://www.w3.org/2000/svg" fill="#ffffff"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <g id="Group_65" data-name="Group 65" transform="translate(-831.568 -384.448)"> <path id="Path_57" data-name="Path 57" d="M833.068,460.252a1.5,1.5,0,0,1-1.061-2.561l33.557-33.56a2.53,2.53,0,0,0,0-3.564l-33.557-33.558a1.5,1.5,0,0,1,2.122-2.121l33.556,33.558a5.53,5.53,0,0,1,0,7.807l-33.557,33.56A1.5,1.5,0,0,1,833.068,460.252Z" fill="#ffffff"></path> </g> </g></svg>
                         </div>
                     </div>
                     <div
